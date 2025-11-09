a2d5435b86d42bad3ff602f63cd4f4dbca2733af
Muhammad Milon <mmilon82814@gmail.com>
2025-11-09 18:16:01 +0600

    Successfully installed all dependency

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './index.css'
+
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-6 rounded-lg">
+      Hello Tailwind v4 in React!
+    </div>
+  )
+}
+export default App;
